d45488ccb8402917e7ec86cd8448698aad9f2ac3
imanmansour86 <imanmansour86@gmail.com>
2022-01-28 20:30:11 -0800

    adding route to read notes from json file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //create the server
 const express = require("express");
 var path = require("path");
+const fs = require("fs");
+const util = require("util");
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
 
 //configure the app to use express and port
 const app = express();
@@ -11,8 +15,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "Develop/public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("Develop/db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 //configure app to listen on specified port above
